6286ff5ed15a0b4ecb2b33bdc050d0fc73461cdc
LucasMendes013 <104436533+LucasMendes013@users.noreply.github.com>
2022-06-15 12:45:35 -0300

    c

--- script_TransversingEManipulacao.js
+++ script_TransversingEManipulacao.js
@@ -8,6 +8,26 @@
 // console.log(menu.innerHTML = '<p>Texto<p>')  //a tag é renderizada 
 
 
-const lista = document.querySelector('.animais-lista')
+// const lista = document.querySelector('.animais-lista')
 
-console.log(lista.children[--lista.children.length] )
+// console.log(lista.children[--lista.children.length] )
+
+
+
+//Duplique o menu e adicione ele em copy
+const menu = document.querySelector('.menu')
+const cloneMenu = menu.cloneNode(true)
+const copy = document.querySelector('.copy')
+copy.appendChild(cloneMenu)
+console.log(menu,copy)
+
+//Selecione o primeiro DT da DL de FAQ
+const dt = document.querySelector('.faq-lista')
+const dl = dt.children[0]
+const dd = dl.nextElementSibling
+console.log(dd)
+
+//Substitua o conteudo html de .faq pelo de .animais
+const faq = document.querySelector('.faq')
+const animais = document.querySelector('.animais-descricao')
+faq.innerHTML = animais.innerHTML
